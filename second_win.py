32ed7a997852dc8addfda897ea162a9cf42ff4fb
arewel1 <sergiobugakov@gmail.com>
2025-05-11 14:49:38 +0300

    Add files via upload
    добавление двух последних таймеров

--- second_win.py
+++ second_win.py
@@ -30,6 +30,43 @@ class TestWin(QWidget):
         self.text_timer.setStyleSheet("color: rgb(0, 0, 0)")
         if time.toString("hh:mm:ss") == "00:00:00":
             self.timer.stop()
+    
+    def timer_sits(self):
+        global time
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+    
+    def timer2Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0, 0, 0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+    
+    def timer_final(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1500)
+    
+    def timer3Event(self):
+        global time
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+            self.text_timer.setStyleSheet("color: rgb(0, 255, 0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.text_timer.setStyleSheet("color: rgb(0, 255, 0)")
+        else :
+            self.text_timer.setStyleSheet("color: rgb(0, 0, 0)")
+        self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0, 0, 0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
 
     def set_appear(self):
         self.setWindowTitle(txt_title)
@@ -100,9 +137,9 @@ class TestWin(QWidget):
 
     def connects(self):
         self.bnt_next.clicked.connect(self.next_click)
-        self.test1.clicked.connect(self.timer_test)
-        self.test2.clicked.connect(self.next_click)
-        self.test3.clicked.connect(self.next_click)
+        self.test3.clicked.connect(self.timer_test)
+        self.test2.clicked.connect(self.timer_sits)
+        self.test1.clicked.connect(self.timer_finel)
     
     def next_click(self):
         self.hide()
